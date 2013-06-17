501584317874754f231894708587244419a75251
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-17 13:56:03 +0200

    feature: add schema and use z3cform

--- collective/portlet/favoriting/favoritingportlet.py
+++ collective/portlet/favoriting/favoritingportlet.py
@@ -1,18 +1,15 @@
 from zope.interface import implements
+from zope import schema
 
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
+from plone.app.portlets.browser import z3cformhelper
 
-# TODO: If you define any fields for the portlet configuration schema below
-# do not forget to uncomment the following import
-#from zope import schema
-from zope.formlib import form
+from z3c.form import field
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-# TODO: If you require i18n translation for any of your schema fields below,
-# uncomment the following to import your package MessageFactory
-#from collective.portlet.favoriting import FavoritingPortletMessageFactory as _
+from collective.portlet.favoriting import FavoritingPortletMessageFactory as _
 
 
 class IFavoritingPortlet(IPortletDataProvider):
@@ -23,15 +20,13 @@ class IFavoritingPortlet(IPortletDataProvider):
     same.
     """
 
-    # TODO: Add any zope.schema fields here to capture portlet configuration
-    # information. Alternatively, if there are no settings, leave this as an
-    # empty interface - see also notes around the add form and edit form
-    # below.
-
-    # some_field = schema.TextLine(title=_(u"Some field"),
-    #                              description=_(u"A field to use"),
-    #                              required=True)
-
+    title = schema.TextLine(title=_(u"Title"), required=False)
+    limit = schema.Int(title=_(u"Limit"), required=False)
+    ftype = schema.Choice(
+        title=_(u"Filter by type"),
+        required=False,
+        vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes",
+    )
 
 class Assignment(base.Assignment):
     """Portlet assignment.
@@ -42,23 +37,23 @@ class Assignment(base.Assignment):
 
     implements(IFavoritingPortlet)
 
-    # TODO: Set default values for the configurable parameters here
+    title = None
+    limit = None
+    ftype = None
 
-    # some_field = u""
-
-    # TODO: Add keyword parameters for configurable parameters here
-    # def __init__(self, some_field=u""):
-    #    self.some_field = some_field
-
-    def __init__(self):
-        pass
+    def __init__(self, title=None, limit=None, ftype=None):
+        self._title = title
+        self.limit = limit
+        self.ftype = ftype
 
     @property
     def title(self):
         """This property is used to give the title of the portlet in the
         "manage portlets" screen.
         """
-        return "Favoriting Portlet"
+        if self._title is None:
+            return _(u"Favorites")
+        return self._title
 
 
 class Renderer(base.Renderer):
@@ -72,38 +67,23 @@ class Renderer(base.Renderer):
     render = ViewPageTemplateFile('favoritingportlet.pt')
 
 
-class AddForm(base.AddForm):
+class AddForm(z3cformhelper.AddForm):
     """Portlet add form.
 
     This is registered in configure.zcml. The form_fields variable tells
     zope.formlib which fields to display. The create() method actually
     constructs the assignment that is being added.
     """
-    form_fields = form.Fields(IFavoritingPortlet)
+    form_fields = field.Fields(IFavoritingPortlet)
 
     def create(self, data):
         return Assignment(**data)
 
 
-# NOTE: If this portlet does not have any configurable parameters, you
-# can use the next AddForm implementation instead of the previous.
-
-# class AddForm(base.NullAddForm):
-#     """Portlet add form.
-#     """
-#     def create(self):
-#         return Assignment()
-
-
-# NOTE: If this portlet does not have any configurable parameters, you
-# can remove the EditForm class definition and delete the editview
-# attribute from the <plone:portlet /> registration in configure.zcml
-
-
-class EditForm(base.EditForm):
+class EditForm(z3cformhelper.EditForm):
     """Portlet edit form.
 
     This is registered with configure.zcml. The form_fields variable tells
     zope.formlib which fields to display.
     """
-    form_fields = form.Fields(IFavoritingPortlet)
+    form_fields = field.Fields(IFavoritingPortlet)
